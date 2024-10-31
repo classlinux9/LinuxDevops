7c288e7ea86342d900ba3fe7aacb9ba4320ad3e4
Amah Nzenje Ivo <ivoamah@gmail.com>
2024-10-31 16:13:37 -0500

    updated script named greeting.sh to remote ropesitory

--- greeting.sh
+++ greeting.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # script to display a greeting  message
- 
+echo "My first and lastname script"
 echo "What is your firstname?"
 read firstname
 echo "Hello $firstname"
 echo "What is your lastname?"
 read lastname
 echo "Hello, $firstname $lastname, How are you doing today?"
-
